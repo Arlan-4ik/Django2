e234c7a6775a1bbbc1b8eed030319465f090a2d9
Arlan-4ik <arlanboraev125@gmail.com>
2025-11-21 18:26:22 +0500

    arlan

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,15 @@
+from django.template import loader
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from bboard.models import Bboard
+
 
 def index(request):
-    return HttpResponse("Привет, бобры!")
+    template = loader.get_template('index.html')
+    bbs = Bboard.objects.all()
+    context = {'bbs': bbs}
+
+
+    return render(request, 'index.html', context)
